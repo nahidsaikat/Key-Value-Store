d43d9957d82a879e312684e91fabd1613fc40290
nahidsaikat <nahidsaikatft40@gmail.com>
2019-12-29 08:09:04 +0600

    Add view classes to key_value_store app

--- key_value_store/views.py
+++ key_value_store/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import KeyValueSerializer
+from .models import KeyValue
+
+
+class KeyValueListCreateAPIView(ListCreateAPIView):
+    queryset = KeyValue.objects.all().order_by('-id')
+    serializer_class = KeyValueSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_context(self):
+        contest = super().get_serializer_context()
+        contest['request'] = self.request
+        return contest
+
+
+class KeyValueRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = KeyValue.objects.all()
+    serializer_class = KeyValueSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_context(self):
+        contest = super().get_serializer_context()
+        contest['request'] = self.request
+        return contest
+
+    def perform_update(self, serializer):
+        serializer.save(updated_by=self.request.user)
